403f237a2263481e2057333c4dc4d79b62f80834
Ruben Wihler <wihlerruben@gmail.com>
2025-01-29 16:58:24 +0100

    refactor shader parse + displaying blue/red rectangle with 2 differents shaders

--- projects/triangle_opengl/src/main.c
+++ projects/triangle_opengl/src/main.c
@@ -26,31 +26,39 @@ int main(void)
     //Affichage des versions
     print_version();
         
-
-
     //un vertex par ligne
     //1 seule attribut: 2 float - position xy
-    const float triangle_vertices[6] = {
-         0.0f,  0.5f, 
-        -0.5f, -0.5f,
+    const float triangle_vertices[] = {
+        -0.5f, -0.5f, 
          0.5f, -0.5f,
+         0.5f,  0.5f,
+        
+        -0.5f, -0.5f, 
+         0.5f,  0.5f,
+        -0.5f,  0.5f,
     };
 
+    //Création du buffer
     unsigned int buffer_id;
     glGenBuffers(1, &buffer_id);
     glBindBuffer(GL_ARRAY_BUFFER, buffer_id);
-    glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), triangle_vertices, GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, 2 * 6 * sizeof(float), triangle_vertices, GL_STATIC_DRAW);
 
-    //attributs
+    //Attribut 0: position
     glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), 0);
     glEnableVertexAttribArray(0); // on active cette attribut
     
-    shader_program_source_t* shader_source_code = 
-        shader_program_source_parse("res/shaders/basic");
+    //Chargement des shaders
+    shader_program_source_t* shader_source_code_basic = shader_program_source_parse("res/shaders/basic");
+    shader_program_source_t* shader_source_code_red = shader_program_source_parse("res/shaders/red");
 
-    unsigned int shader_program = create_shader_program(shader_source_code->vertex,
-                                                        shader_source_code->fragment);
-    glUseProgram(shader_program);
+    //Création des programmes a partir des sources
+    unsigned int shader_program_basic = create_shader_program(shader_source_code_basic->vertex, shader_source_code_basic->fragment);
+    unsigned int shader_program_red   = create_shader_program(shader_source_code_basic->vertex, shader_source_code_red->fragment);
+
+    //Libération de la mémoire des sources
+    shader_program_source_destroy(shader_source_code_basic);
+    shader_program_source_destroy(shader_source_code_red);
 
     //Temps que la fenêtre n'est pas fermée
     while (!glfwWindowShouldClose(window))
@@ -59,8 +67,13 @@ int main(void)
         //(en gros on dessine un fond noir)
         glClear(GL_COLOR_BUFFER_BIT);
             
-        //On fais le draw call sur le buffer bindé
+        //dessine le triangle bleu
+        glUseProgram(shader_program_basic);
         glDrawArrays(GL_TRIANGLES, 0, 3);
+
+        //dessine le triangle rouge
+        glUseProgram(shader_program_red);
+        glDrawArrays(GL_TRIANGLES, 3, 3);
 
         //On échange les buffers
         //C'est à dire qu'on affiche le buffer ou l'on a dessiné
@@ -70,11 +83,11 @@ int main(void)
         //On gère les événements
         glfwPollEvents();
     }
-    
-
 
     //On libère la mémoire
-    glDeleteProgram(shader_program);
+    glDeleteProgram(shader_program_basic);
+    glDeleteProgram(shader_program_red);
+    glDeleteBuffers(1, &buffer_id);
     glfwTerminate();
 
     return 0;
--- projects/triangle_opengl/src/shaders/shader_parser.c
+++ projects/triangle_opengl/src/shaders/shader_parser.c
@@ -1,26 +1,38 @@
+// Author: Ruben Wihler
+// Date: 2025-01-29
+
+/*
+*   Ce fichier contient les fonctions de lecture des fichiers de shader.
+*   Les fichiers de shader sont des fichiers texte contenant le code source des shaders OpenGL (GLSL).
+*   Pour l'instant, seuls les shaders de type vertex et fragment sont supportés.
+*
+*   Organisation des fichiers de shader:
+*   - Chaque paire de fichiers de shader (vertex et fragment) est placée dans un dossier qui porte le nom du shader.
+*   - Les fichiers de shader doivent être nommés `vert.glsl` et `frag.glsl`. (en tout cas pour l'instant voir #defines)
+*   - Exemple:
+*        - res/shaders/basic/
+*            - vert.glsl
+*            - frag.glsl
+*
+*   Note:
+*   Il est possible de ne pas avoir de fichier vertex ou fragment. Cela provoquera un warning en mode de compilation DEBUG.
+*/
 
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <assert.h>
+
 #include "shader_parser.h"
 
 #define SHADER_FILE_EXT ".glsl"
 #define SHADER_FILE_NAME_FRAGMENT "frag"
 #define SHADER_FILE_NAME_VERTEX "vert"
 
+static char* get_file_content(const char* filepath);
+
 shader_program_source_t* shader_program_source_parse(const char* dirpath)
 {
-    /*
-    * Cette fonction retourne un pointeur vers une structure contenant les sources des shaders
-    * La memoire allouée est contigue. La structure est suivie des deux chaines de caractères.
-    * 
-    * 2*4 bytes pour les pointeurs
-    * contenu du fichier vertex + 1 pour le '\0'
-    * contenu du fichier fragment + 1 pour le '\0'
-    *
-    * il est de la responsabilité de l'appelant de libérer la mémoire
-    */
-
     //calcul des tailles des chemins
     size_t pathlen, vert_pathlen, frag_pathlen;
     pathlen = strlen(dirpath) + strlen(SHADER_FILE_EXT) + 2;//+2 pour le '/' et le '\0'
@@ -29,8 +41,8 @@ shader_program_source_t* shader_program_source_parse(const char* dirpath)
     
     //allocation sur la stack des chemins
     char *vertpath, *fragpath;
-    vertpath = alloca(sizeof(char) * vert_pathlen);
-    fragpath = alloca(sizeof(char) * frag_pathlen);
+    vertpath = (char*)alloca(vert_pathlen * sizeof(*vertpath));
+    fragpath = (char*)alloca(frag_pathlen * sizeof(*fragpath));
     
     //concatenation des chemins
     strcpy(vertpath, dirpath);
@@ -40,65 +52,80 @@ shader_program_source_t* shader_program_source_parse(const char* dirpath)
     vertpath[vert_pathlen] = '\0';
     fragpath[frag_pathlen] = '\0';
     
-    //ouverture des fichiers
-    FILE *vertfile, *fragfile;
-    vertfile = fopen(vertpath, "r");
-    fragfile = fopen(fragpath, "r");
+    //allocation du shader_program_source_t
+    shader_program_source_t *program = (shader_program_source_t*)malloc(sizeof(*program));
+    if(!program) return (perror("malloc"), NULL);
     
-    //gestion des erreurs
-    if(!vertfile || !fragfile)
+    //lecture des fichiers
+    program->vertex = get_file_content(vertpath);
+    program->fragment = get_file_content(fragpath);
+
+    #ifdef DEBUG
+    
+    if(!program->vertex)
+        fprintf(stderr, "Cannot read vertex shader file: %s\n", vertpath);
+
+    if(!program->fragment)
+        fprintf(stderr, "Cannot read fragment shader file: %s\n", fragpath);
+
+    if(!program->vertex || !program->fragment)
     {
-        fprintf(stderr, "error while oppening shaders file!\n");
-        return NULL;
+        fprintf(stderr, "shader_program_source_parse: "
+                "Some shaders files are set to NULL. This may be intentional.\n");
     }
-    
-    //calcul des tailles des fichiers
-    long vert_file_length, frag_file_length;
-    fseek(vertfile, 0, SEEK_END);
-    fseek(fragfile, 0, SEEK_END);
-    vert_file_length = ftell(vertfile);
-    frag_file_length = ftell(fragfile);
-    fseek(vertfile, 0, SEEK_SET);
-    fseek(fragfile, 0, SEEK_SET);
 
-    //allocation du buffer
-    size_t buffer_len = vert_file_length + frag_file_length + 2;
-    void *buffer = malloc(sizeof(shader_program_source_t) + (sizeof(char) * buffer_len));
-    char *srcbuffer = (char*)(((shader_program_source_t*)buffer)+1);
+    #endif
 
-    size_t i = 0;
-    char c = 0;
-    
-    //lecture du fichier vertex
-    char *vertbuffer = srcbuffer;
-    while((c = fgetc(vertfile)) != EOF)
-        srcbuffer[i++] = c;
-    srcbuffer[i++] = '\0';
-    
-    //lecture du fichier fragment
-    char *fragbuffer = srcbuffer+i;
-    while((c = fgetc(fragfile)) != EOF)
-        srcbuffer[i++] = c;
-    srcbuffer[i] = '\0';
-    
-    //fermeture des fichiers
-    fclose(vertfile);
-    fclose(fragfile);
-
-    //remplissage de la structure
-    *((shader_program_source_t*)buffer) = (shader_program_source_t){
-        .vertex = vertbuffer,
-        .fragment = fragbuffer
-    };
-    
-    return buffer;
+    return program;
 }
+
+void shader_program_source_destroy(shader_program_source_t* src)
+{
+    assert(src && "shader_program_source_destroy: src to destroy is NULL");
+
+    free((void*)src->vertex);
+    free((void*)src->fragment);
+    free(src);
+}
+
+/// Retourne NULL en cas d'erreur.
+/// Le contenu du fichier est alloué sur le tas et doit être libéré.
+static char* get_file_content(const char* filepath)
+{
+    //ouverture du fichier
+    FILE *file = fopen(filepath, "r");
+    if(!file) return NULL;
+
+    //calcul de la taille du fichier
+    fseek(file, 0, SEEK_END);
+    size_t length = ftell(file);
+    fseek(file, 0, SEEK_SET);
+    
+    //allocation du contenu
+    char* content = (char*)malloc(length * sizeof(char));
+    if(!content) return (perror("malloc"), fclose(file), NULL);
+    
+    //lecture du fichier
+    size_t i = 0; char c;
+    while((c = fgetc(file)) != EOF)
+        content[i++] = c;
+    content[i] = '\0';
+
+    //fermeture du fichier
+    fclose(file);
+    return content;
+}
+
 
 void print_shader_program_source(shader_program_source_t *src)
 {
+    if(!src)
+    {
+        printf("(shader_program_source_t)@{NULL}\n");
+        return;
+    }
+
     printf("(shader_program_source_t)@{%p}\n", src);
-    printf("vertex:                  @{%p}\n", src->vertex);
-    printf("fragment:                @{%p}\n", src->fragment);
-    printf("vertex as string:\n%s\n", src->vertex);
-    printf("fragment as string:\n%s\n", src->fragment);
+    printf("vertex:\n%s\n", src->vertex);
+    printf("fragment:\n%s\n", src->fragment);
 }
