117282cd6f87e5da98dfd1523607e5ca8312fa8b
Ruben Wihler <wihlerruben@gmail.com>
2025-02-19 14:09:42 +0100

    replaced game api func type to GLFW ones

--- projects/opengl_project/src/gllib/game/game.h
+++ projects/opengl_project/src/gllib/game/game.h
@@ -26,11 +26,11 @@ typedef struct _game_api_t{
     void (*on_clean)(game_t *game);
 
     struct {
-        void (*framebuffer_size_callback)(GLFWwindow *window, int width, int height);
-        void (*key_callback)(GLFWwindow *window, int key, int scancode, int action, int mods);
-        void (*mouse_button_callback)(GLFWwindow *window, int button, int action, int mods);
-        void (*cursor_position_callback)(GLFWwindow *window, double xpos, double ypos);
-        void (*scroll_callback)(GLFWwindow *window, double xoffset, double yoffset);
+        GLFWframebuffersizefun framebuffer_size_callback;
+        GLFWkeyfun key_callback;
+        GLFWmousebuttonfun mouse_button_callback;
+        GLFWcursorposfun cursor_position_callback;
+        GLFWscrollfun scroll_callback;
     } glfw_callbacks;
 
 } game_api_t;
